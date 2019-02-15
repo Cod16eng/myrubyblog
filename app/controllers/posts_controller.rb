538ad76a8f365351510116a2fc8104608a0856f1
Codrin Popescu <codrin@ascheri.co.uk>
2019-02-15 13:39:29 +0000

    migration

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+	
+	def index
+		@content_first = 'This is a sample text form my ruby blog';
+		@content_second = 'This is another sample text form my ruby blog';
+		
+	end
+
+	def create
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def show
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+end
